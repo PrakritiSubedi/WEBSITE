d46f79fc6b3ce9932d619536077afc9f728c594d
PrakritiSubedi <107305923+PrakritiSubedi@users.noreply.github.com>
2025-02-06 16:53:37 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,47 +1,40 @@
-
-// Sticky Navigation Menu
-let nav = document.querySelector("nav");
-let scrollBtn = document.querySelector(".scroll-button a");
-
-// Show/hide sticky navigation and scroll button based on scroll position
-window.onscroll = function () {
-  if (document.documentElement.scrollTop > 20) {
-    nav.classList.add("sticky");
-    scrollBtn.style.display = "block";
-  } else {
-    nav.classList.remove("sticky");
-    scrollBtn.style.display = "none";
-  }
-};
-
-// Side Navigation Menu
-let body = document.querySelector("body");
-let navBar = document.querySelector(".navbar");
-let menuBtn = document.querySelector(".menu-btn");
-let cancelBtn = document.querySelector(".cancel-btn");
-
-// Open side navigation
-menuBtn.onclick = function () {
-  navBar.classList.add("active");
-  menuBtn.style.opacity = "0";
-  menuBtn.style.pointerEvents = "none";
-  body.style.overflow = "hidden";
-  scrollBtn.style.pointerEvents = "none";
-};
-
-const hideNavMenu = () => {
-  navBar.classList.remove("active");
-  menuBtn.style.opacity = "1";
-  menuBtn.style.pointerEvents = "auto";
-  body.style.overflow = "auto";
-  scrollBtn.style.pointerEvents = "auto";
-};
-
-// Close side navigation
-cancelBtn.onclick = hideNavMenu;
-
-// Close side navigation when a menu link is clicked
-let navLinks = document.querySelectorAll(".menu li a");
-navLinks.forEach((link) => {
-  link.addEventListener("click", hideNavMenu);
-});
+function hamburg(){
+    const navbar = document.querySelector(".dropdown")
+    navbar.style.transform = "translateY(0px)"
+}
+function cancel(){
+    const navbar = document.querySelector(".dropdown")
+    navbar.style.transform = "translateY(-500px)"
+}
+// Typewriter Effect
+const texts = [
+    "DEVELOPER",
+    "DESIGNER",
+    "YOUTUBER"
+]
+let speed  =100;
+const textElements = document.querySelector(".typewriter-text");
+let textIndex = 0;
+let charcterIndex = 0;
+function typeWriter(){
+    if (charcterIndex < texts[textIndex].length){
+        textElements.innerHTML += texts[textIndex].charAt(charcterIndex);
+        charcterIndex++;
+        setTimeout(typeWriter, speed);
+    }
+    else{
+        setTimeout(eraseText, 1000)
+    }
+}
+function eraseText(){
+    if(textElements.innerHTML.length > 0){
+        textElements.innerHTML = textElements.innerHTML.slice(0,-1);
+        setTimeout(eraseText, 50)
+    }
+    else{
+        textIndex = (textIndex + 1) % texts.length;
+        charcterIndex = 0;
+        setTimeout(typeWriter, 500)
+    }
+}
+window.onload = typeWriter
