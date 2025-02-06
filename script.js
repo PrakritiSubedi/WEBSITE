655a610f9cfce2f6258e13228f9849bfdcca7d22
PrakritiSubedi <107305923+PrakritiSubedi@users.noreply.github.com>
2025-02-06 19:57:37 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,56 +1,44 @@
+// Dropdown functionality
 function hamburg() {
     const navbar = document.querySelector(".dropdown");
-    if (navbar) {
-        navbar.style.transform = "translateY(0px)";
-    } else {
-        console.error("Dropdown element not found.");
-    }
+    navbar.style.transform = "translateY(0px)";
+    document.querySelector(".hamburg").style.display = "none";
+    document.querySelector(".cancel").style.display = "block";
 }
 
 function cancel() {
     const navbar = document.querySelector(".dropdown");
-    if (navbar) {
-        navbar.style.transform = "translateY(-500px)";
-    } else {
-        console.error("Dropdown element not found.");
-    }
+    navbar.style.transform = "translateY(-500px)";
+    document.querySelector(".cancel").style.display = "none";
+    document.querySelector(".hamburg").style.display = "block";
 }
 
 // Typewriter Effect
-const texts = [
-    "DEVELOPER",
-    "DESIGNER",
-    "YOUTUBER"
-];
+const texts = ["DEVELOPER", "DESIGNER", "YOUTUBER"];
 let speed = 100;
 const textElements = document.querySelector(".typewriter-text");
+let textIndex = 0;
+let charcterIndex = 0;
 
-if (textElements) {
-    let textIndex = 0;
-    let characterIndex = 0;
-
-    function typeWriter() {
-        if (characterIndex < texts[textIndex].length) {
-            textElements.innerHTML += texts[textIndex].charAt(characterIndex);
-            characterIndex++;
-            setTimeout(typeWriter, speed);
-        } else {
-            setTimeout(eraseText, 1000);
-        }
+function typeWriter() {
+    if (charcterIndex < texts[textIndex].length) {
+        textElements.innerHTML += texts[textIndex].charAt(charcterIndex);
+        charcterIndex++;
+        setTimeout(typeWriter, speed);
+    } else {
+        setTimeout(eraseText, 1000);
     }
-
-    function eraseText() {
-        if (textElements.innerHTML.length > 0) {
-            textElements.innerHTML = textElements.innerHTML.slice(0, -1);
-            setTimeout(eraseText, 50);
-        } else {
-            textIndex = (textIndex + 1) % texts.length;
-            characterIndex = 0;
-            setTimeout(typeWriter, 500);
-        }
-    }
-
-    window.onload = typeWriter;
-} else {
-    console.error("Typewriter element not found.");
 }
+
+function eraseText() {
+    if (textElements.innerHTML.length > 0) {
+        textElements.innerHTML = textElements.innerHTML.slice(0, -1);
+        setTimeout(eraseText, 50);
+    } else {
+        textIndex = (textIndex + 1) % texts.length;
+        charcterIndex = 0;
+        setTimeout(typeWriter, 500);
+    }
+}
+
+window.onload = typeWriter;
